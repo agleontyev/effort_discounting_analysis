deac4c798b101e3809637d2b022e6a007e9a4609
Anton Leontyev <a.g.leontiev@gmail.com>
2019-07-31 10:21:56 -0500

    Add files via upload

---
+++ mle_class.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 17:28:42 2019
+
+@author: agleo
+"""
+
+import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+
+path = r"C:\Users\agleo\Dropbox\Anton\mock data (McG).xlsx"
+df = pd.read_excel(path, index_col = 0)
+df['SubjID'] = 777
+data_all_subjects = df
+initParams = [0.5,0.5]
+
+
+class LinearDiscountMLE:
+    
+    def __init__(self, initial_params):
+        self.initial_params = initial_params
+        
+    def deldisc(self,initial_params, df):
+        r1 = df['o1_r'].values
+        e1 = df['o1_e'].values
+        r2 = df['o2_r'].values
+        e2 = df['o2_e'].values
+        choice = df['choice'].values
+
+        chose_1 = choice == 1
+
+        k = initial_params[0]
+        beta = initial_params[1]   
+        svi = r2 - k*e2
+        svb = r1 - k*e1
+        esvi = np.exp(beta*svi)
+        esvb = np.exp(beta*svb)
+        softmax = esvi / (esvi + esvb)
+        softmax[chose_1] =  1 - softmax[chose_1]
+        
+        LL = -np.sum(np.log(softmax))
+
+        return LL
+        
+    def optifun(self, df):
+        bnds = ((0, 1), (None, None))
+        results = minimize(self.deldisc, x0 = self.initial_params, bounds =bnds, args=(df,))
+        return results
+    
+    def estimate_parameters(self, df):
+       finres = df.groupby('SubjID').apply(self.optifun)
+       return finres
+
+
+linear_discount_mle = LinearDiscountMLE(initial_params=initParams)
+
+result = linear_discount_mle.estimate_parameters(data_all_subjects)
+
+
+print('k=',str(result.values[0].x[0]))
+print('beta=',str(result.values[0].x[1]))
